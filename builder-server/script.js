1ad7de63175baa8b527f482c27660ec39382cc6c
Girish-del <girishnalawade01@gmail.com>
2024-12-18 15:26:34 -0700

    Made changes to push files into the AWS s3 bucket. Access keys are kept private

--- builder-server/script.js
+++ builder-server/script.js
@@ -1,6 +1,20 @@
 const { exec } = require('child_process')
 const path = require('path')
 const fs = require('fs')
+const { S3Client, PutObjectCommand} = require('@aws-sdk/client-s3')
+const mime = require('mime-types')
+
+
+// Creating an S3 client
+const s3Client = new S3Client({
+    region: 'us-east-1',
+    credentials: {
+        accessKeyId: '',
+        secretAccessKey: ''
+    }
+})
+
+const PROJECT_ID = process.env.PROJECT_ID
 
 async function init(){
     console.log("script.js Executing started")
@@ -25,7 +39,18 @@ async function init(){
         for(const filePath of distFolderContents){
             if(fs.lstatSync(filePath).isDirectory()) continue;
 
-            
+            console.log("Uploading..... ", filePath)
+
+            const command = new PutObjectCommand({
+                Bucket: 'vercel-clone-project-01',
+                Key: `__outputs/${PROJECT_ID}/${filePath}`,
+                Body: fs.createReadStream(filePath),
+                ContentType: mime.lookup(filePath)
+            })
+
+            await s3Client.send(command)
+            console.log("Uploaded.....", filePath)
         }
+        console.log('Done....')
     })
 }
